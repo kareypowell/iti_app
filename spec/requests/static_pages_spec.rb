6607890853338d8277a0e011535dc214ebda0feb
kareypowell <kareyp9@gmail.com>
2013-10-28 15:50:27 -0500

    Add about action to StaticPages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,28 +2,28 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the contanet 'ITI'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('ITI')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Intellectual Technologies and Investments")
-    end
+    it { should have_content('ITI') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
 end
